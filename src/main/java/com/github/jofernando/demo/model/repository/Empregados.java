253b63bcf826b9af9dd8d80c0a753c4d09a17ed2
José Fernando Mendes <fmdinho3@gmail.com>
2018-01-09 10:25:22 -0300

    Adiciona repositorios das entidades

---
+++ src/main/java/com/github/jofernando/demo/model/repository/Empregados.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.jofernando.demo.model.repository;
+
+import com.github.jofernando.demo.model.entidades.Empregado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author fernando
+ */
+public interface Empregados extends JpaRepository<Empregado, Long> {
+
+}
